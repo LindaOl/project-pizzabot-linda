3d9688992f97a1c7199982f1d1cba5d020317f6e
LindaOl <183224471+lillesh@users.noreply.github.com>
2024-11-24 16:19:47 +0100

    added promt for customer name

--- code/script.js
+++ code/script.js
@@ -4,10 +4,23 @@
 // Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+
+const customerName = prompt("Pleace enter your name");
+console.log(customerName);
+
+if (customerName != null) {
+  alert("Welcome " + customerName + "! Please continue to make your order.")
+} else {
+  alert("An error has occured. Refresh the page.")
+};
+
+
 
 // Step 2 - Food choice
 // Your code goes here
+
+
 
 // Step 3 - Subtype choice
 // Your code goes here
